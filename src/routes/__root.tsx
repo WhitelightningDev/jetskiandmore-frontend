3906653475855d9f728701cf4e836723297097a3
Daniel Mommsen <danielmommsen@hotmail.com>
2025-11-14 19:21:31 +0200

    `Removed TanStackDevtools and TanStackRouterDevtoolsPanel from __root.tsx`

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,4 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
 
 import Header from '../components/Header'
 import ContactFab from '../components/ContactFab'
@@ -13,17 +11,6 @@ export const Route = createRootRoute({
       <Breadcrumbs />
       <Outlet />
       <ContactFab />
-      <TanStackDevtools
-        config={{
-          position: 'bottom-right',
-        }}
-        plugins={[
-          {
-            name: 'Tanstack Router',
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-        ]}
-      />
     </>
   ),
 })
